598e0a930ce9240b83c5c48155fae2b8b93c23ed
BurovSt <stasburov1991@gmail.com>
2024-05-01 17:44:36 +0300

    Routes & URLs - Flask Tutorial Series #2

--- app.py
+++ app.py
@@ -1,42 +1,40 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
+    myvalue = 'NeuralNine'
+    myresults = 10 + 20
+    mylist = [10, 20, 30, 40, 50]
+    return render_template('index.html',
+                           mylist=mylist)
 
-# standard = GET , if want can use GET, POST, PUT, DELETE
-@app.route('/hello')
-def hello():
-    response = make_response('Hello World\n')
-    response.status_code = 202
-    response.headers['content-type'] = 'text/plain'
-    return response
-
-
-@app.route('/greet/<name>')
-def greet(name):
-    return f"Hello {name}"
+@app.route('/other')
+def other():
+    some_text = 'Hello World'
+    return render_template('other.html', some_text=some_text)
 
 
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
 
-    return f"{number1} + {number2} = {number1 + number2}"
+@app.template_filter('repeat')
+def repeat(s, times=2):
+    return s * times
 
-
-@app.route("/handle_url_params")
-def hande_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args['greeting']
-        name = request.args.get('name')
-        return f'{greeting}, {name}'
-    else:
-        return 'Some parameters are missing'
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return "".join([c.upper() if i % 2 == 0 else c.lower()
+                    for i, c in enumerate(s)])
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
+
 
